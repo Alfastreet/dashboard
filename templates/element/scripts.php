709eb3efa1b6c434f1d92b43421564c06e5f2aa3
Nicolas Cuadros <ingenieria@alfastreet.co>
2022-10-12 10:42:59 -0500

    Complementacion del nuevos diseños bootstrap

--- templates/element/scripts.php
+++ templates/element/scripts.php
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
 <?= $this->Html->script('https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js') ?>
 <?= $this->Html->script('https://code.jquery.com/jquery-3.6.1.js') ?>
 <?= $this->Html->script('/vendors/@coreui/coreui/js/coreui.bundle.min.js') ?>
